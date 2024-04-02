67245c948c2d8e176376f193fdd66797087fa3c2
Hennadii71 <genadisema@gmail.com>
2024-04-02 16:19:21 +0300

    Update

---
+++ js/exsp.js
@@ -0,0 +1,12 @@
+const sum = (a, b)=>{
+    return a+b;
+};
+
+const grup = (x, y)=>{
+    return x*y;
+}
+
+export default {
+    sum,
+    grup
+};
---
+++ js/imp.js
@@ -0,0 +1,3 @@
+import  fun from "./exsp.js";
+
+console.log(fun.sum(1,2));
